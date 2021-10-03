b1376bedf4c44f048068433bc5a510e54270c25b
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-03 20:37:57 -0300

    Funcao 'feito' funcionando

--- frontend/src/pages/home/linha/Linha.jsx
+++ frontend/src/pages/home/linha/Linha.jsx
@@ -5,14 +5,27 @@ const Linha = ({ dadosLinha, updateTarefas }) => {
         })
         await updateTarefas();
     }
+    const mudarFeito = async (feito) => {
+        await fetch(`https://todolist-mauricio.herokuapp.com/lists/patch?id=${dadosLinha.id}`, {
+            method: "PATCH",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "feito": feito
+            })
+        })
+        await updateTarefas();
+    }
     return (
-        <tr key={dadosLinha.id}>
+        <tr key={dadosLinha.id} className={dadosLinha.feito === false ? "incompleta" : "completa"}>
             <td>{dadosLinha.datainicio}</td>
             <td>{dadosLinha.tarefa}</td>
             <td>{dadosLinha.prazofinal}</td>
             <td>
                 <button>E</button>
-                <button>F</button>
+                <button onClick={() => mudarFeito(!dadosLinha.feito)}>F</button>
                 <button onClick={excluirTarefa}>Ex</button>
             </td>
         </tr>
