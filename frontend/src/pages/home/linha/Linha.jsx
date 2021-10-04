94a493cbb1de850a2138c955d7c70708ef5ef387
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-04 16:39:27 -0300

    POST nova tarefa

--- frontend/src/pages/home/linha/Linha.jsx
+++ frontend/src/pages/home/linha/Linha.jsx
@@ -1,4 +1,7 @@
+import './styles.css';
+
 import { BASE_URL } from "../../../utils/config";
+import { formatLocalDate } from "../../../utils/format";
 
 const Linha = ({ dadosLinha, updateTarefas }) => {
     const excluirTarefa = async () => {
@@ -21,10 +24,11 @@ const Linha = ({ dadosLinha, updateTarefas }) => {
         await updateTarefas();
     }
     return (
-        <tr key={dadosLinha.id} className={dadosLinha.feito === false ? "incompleta" : "completa"}>
-            <td>{dadosLinha.datainicio}</td>
+        // eslint-disable-next-line react/jsx-no-duplicate-props
+        <tr key={dadosLinha.id} className={`c-linha ${dadosLinha.feito === false ? "incompleta" : "completa"}`}>
+            <td>{formatLocalDate(dadosLinha.datainicio, "dd/MM/yyyy")}</td>
             <td>{dadosLinha.tarefa}</td>
-            <td>{dadosLinha.prazofinal}</td>
+            <td>{formatLocalDate(dadosLinha.prazofinal, "dd/MM/yyyy")}</td>
             <td>
                 <button>E</button>
                 <button onClick={() => mudarFeito(!dadosLinha.feito)}>F</button>
