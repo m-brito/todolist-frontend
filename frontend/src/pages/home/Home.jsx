79b59b7d77a2d1202a357cd0007f8599a6299657
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-04 11:38:33 -0300

    Configuracao de variavel de ambiente

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({ ip }) => {
   }, []);
 
   async function getTarefas() {
-    console.log(BASE_URL)
+    console.log(process.env)
     const resp = await fetch(`${BASE_URL}/lists/get-ip?ip=${ip}`, {
         "method": "GET"
     });
